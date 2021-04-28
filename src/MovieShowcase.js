a8e175b6ee519437c55fc9d1d9a84a0701ce667f
Egregious <dvdscheibel@yahoo.com>
2021-04-28 09:18:17 -0400

    Completed

--- src/MovieShowcase.js
+++ src/MovieShowcase.js
@@ -1,14 +1,21 @@
 import React, { Component } from 'react';
-import MovieCard from './card-components/MovieCard.js'
-import movieData from './data.js'
+import MovieCard from './card-components/MovieCard.js';
+import movieData from './data.js';
 
 export default class MovieShowcase extends Component {
 
   generateMovieCards = () => {
-    // map over your movieData array and return an array of the correct JSX
+    { return movieData.map(movieObj => <MovieCard 
+        key={movieObj.title} 
+        title={movieObj.title} 
+        genres={movieObj.genres} 
+        IMDBRating={movieObj.IMDBRating} 
+        poster={movieObj.poster} 
+    />) }
   }
 
   render() {
+    console.log(movieData)
     return (
       <div id="movie-showcase">
         {this.generateMovieCards()}
